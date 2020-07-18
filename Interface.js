9d000e4f82f4404c1c32c68eb57b47046ced5250
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2020-07-18 14:39:01 -0500

    funcionalidades full

---
+++ Interface.js
@@ -0,0 +1,137 @@
+export default class Interface {
+    constructor() {
+        this.listSong = [];
+        this.loveSong = [];
+        this.playingSong = [];
+    }
+
+    addSong() {
+        this.listSong.push(this.getData())
+    }
+    getData() {
+        let name = document.querySelector("#name").value;
+        let author = document.querySelector("#autor").value;
+        let year = document.querySelector("#year").value;
+        let objData = {
+            name: name,
+            author: author,
+            year: year
+        };
+        return objData;
+    }
+
+    print() {
+        for (let i = 0; i < this.listSong.length; i++) { 
+            let songPrint = document.querySelector("#print");
+            let element = document.createElement("div");
+            element.className = "agregadasBorde";
+            element.innerHTML += `<p class="cancionTitle">Canción</p>
+            <p class="cancionName" id="cancionName">${this.listSong[i].name}</p>
+            <img class="img" src="imgs/musica.png">
+            <div class="agregadasInfo">
+                <p id="cancionInfo">${this.listSong[i].author}-${this.listSong[i].year} </p>
+            </div>
+            <div class="actions">
+                    <button class="btns mover"><img class="logos" id="btnLove" data-loving="${this.listSong[i].name}-${this.listSong[i].author}" name="love" src="imgs/heart.png"></button>
+                    <button  class="btns" id="btnPlay"><img data-suena="${this.listSong[i].name}" data-nombre="Reproduciendo...${this.listSong[i].name}" class="logos" name="play" src="imgs/play.png"></button>
+            </div>
+            <button class="btnDelete"><img class="logoDelete" data-love="${this.listSong[i].name}-${this.listSong[i].author}" data-delete="${this.listSong[i].name}" name="delete" id="delete" src="imgs/delete.png"></button>
+            `;
+            songPrint.appendChild(element)
+        }
+        
+    }
+
+
+    find(data) {
+        let searching = this.listSong.map(search => search.name);
+        for (let index = 0; index < searching.length; index++) {
+            if (searching[index] === data) {
+                return index;
+            }
+
+        }
+    }
+    deleteSong(element) {
+        let loveDelete = element.dataset.love;
+        let findedLove = this.findLove(loveDelete)
+        let finded = this.find(element.dataset.delete);
+        if (element.name == "delete" && finded > -1) {
+            this.listSong.splice(finded, 1);
+            console.log(this.findLove(loveDelete));
+            console.log(findedLove);
+            if(findedLove>-1) {
+                this.loveSong.splice(findedLove,1);
+                document.querySelector("#loving").innerHTML = "";
+                this.printLove();
+            }
+            if(this.loveSong.length == 0) {
+                document.querySelector("#loving").innerHTML = "No hay en este momento";
+            }
+            if(this.playingSong[0] === element.dataset.delete) {
+                this.playingSong.splice(0,1)
+                document.querySelector("#playing").innerHTML = "Sin reproducción"
+            } 
+        }
+      
+        
+    }
+
+    love(element) {
+        if(element.name == "love") {
+            this.loveSong.push(element.dataset.loving)
+            document.querySelector("#loving").innerHTML = "";
+            this.printLove();  
+        }
+    }
+
+    printLove() {
+        let loving = document.querySelector("#loving");
+        for (let i = 0; i < this.loveSong.length; i++) {
+            loving.innerHTML += `<img class="cora" src="imgs/heart.png"> ${this.loveSong[i]} <br> `;
+        } 
+    }
+
+    findLove(data) {
+        let searching = this.loveSong.map(search => search);
+        for (let index = 0; index < searching.length; index++) {
+            if (searching[index] == data) {
+                console.log(searching[index]);
+                return index;
+            }
+        }
+    }
+
+    resetForm() {
+        document.querySelector("#form").reset();
+    }
+
+    play(element) {
+        let playing = document.querySelector("#playing");
+        if(element.name == "play") {
+            console.log(element.dataset.suena);
+            if(this.playingSong.length == 0) {
+                this.playingSong.push(element.dataset.suena)
+                playing.innerHTML = `${element.dataset.nombre} <img class="playing-img" src="imgs/playing.png">`
+            }
+            else{
+                this.playingSong.splice(0,1);
+                this.playingSong.push(element.dataset.suena);
+                playing.innerHTML = `${element.dataset.nombre} <img class="playing-img" src="imgs/playing.png">`
+            }
+        }
+    
+    }
+
+    findPlaying(data) {
+        let searching = this.playingSong.map(search => search);
+        console.log(searching);
+        for (let index = 0; index < searching.length; index++) {
+            if (searching[index] == data) {
+                return index;
+            }
+        }
+    }
+
+    
+}
--- main.js
+++ main.js
@@ -1,101 +1,25 @@
-class Song {
-    constructor(name, author, year) {
-        this.name = name;
-        this.author = author;
-        this.year = year;
+import Interface from "./Interface.js"
+class Main {
+    constructor() {
+        let ui = new Interface();
+        document.querySelector("#btnAgregar").addEventListener("click", () => {
+            document.querySelector("#print").innerHTML = "";
+            ui.addSong();
+            ui.print();
+            ui.resetForm()
+        });
 
+        document.querySelector("#print").addEventListener("click", (event) => {
+            document.querySelector("#print").innerHTML = "";
+            ui.deleteSong(event.target)
+            ui.print();
+         });
+         document.querySelector("#print").addEventListener("click", (event) => {
+            ui.love(event.target);
+         });
+         document.querySelector("#print").addEventListener("click", (event) => {
+            ui.play(event.target);
+         });
     }
 }
-
-class UI {
-    addSong(song) {
-        let songList = document.querySelector("#print");
-        let element = document.createElement("div");
-        element.className = "agregadasBorde"
-        element.innerHTML = `
-                <p class="cancionTitle">Canción</p>
-                <p class="cancionName" id="cancionName">${song.name}</p>
-                <img class="img" src="imgs/musica.png">
-                <div class="agregadasInfo">
-                    <p id="cancionInfo">${song.author}-${song.year}</p>
-                </div>
-                <div class="actions">
-                        <button class="btns mover"><img class="logos" id="btnLove"  data-loving="${song.name}-${song.author}" name="love" src="imgs/heart.png"></button>
-                        <button  class="btns" id="btnPlay"><img data-nombre="Reproduciendo...${song.name}" class="logos" name="play" src="imgs/play.png"></button>
-                </div>
-                <button class="btnDelete"><img class="logoDelete" name="delete" src="imgs/delete.png"></button>
-        `;
-        songList.appendChild(element)
-    }
-
-    resetForm() {
-        document.querySelector("#form").reset();
-    }
-
-
-    playSong(element) {
-        console.log(element);
-        let playing = document.querySelector("#playing");
-        if (element.name == "play") {
-            playing.innerHTML = `${element.dataset.nombre} <img class="playing-img" src="playing.png">`; /* dataset es para poder manejar los datos de los atributos personalizados */
-        }
-    }
-    
-    loveSong(element) {
-        console.log(element.parentElement.parentElement);
-        let loving = document.querySelector("#loving");
-        if(element.name == "love") {
-            loving.innerHTML += `<img class="cora" src="heart.png"> ${element.dataset.loving} <br> `;
-            /* loving.innerHTML = `${this.addLoveSong()}` */
-        }
-    }
-   /*  addLoveSong() {
-        let lovesList = document.querySelector("#cancionesLoves");
-        let element = document.createElement("p");
-        element.innerHTML = `${this.loveSong(element)}`;
-        lovesList.appendChild(element);
-    } */
-    
-    delete(element) {
-        if(element.name == "delete") {
-            element.parentElement.parentElement.remove();
-            this.prints();
-        }
-    }
-
-    prints() {
-        let playing = document.querySelector("#playing");
-        let loving = document.querySelector("#loving");
-        playing.innerHTML = "";
-        loving.innerHTML = "";
-    }
-}
-
-document.querySelector("#btnAgregar").addEventListener("click", () => {
-    let name = document.querySelector("#name").value;
-    let autor = document.querySelector("#autor").value;
-    let year = document.querySelector("#year").value;
-    let song = new Song(name, autor, year)
-    let ui = new UI();
-    ui.addSong(song)
-    ui.resetForm();
-});
-
-document.querySelector("#print").addEventListener("click", (event) => {
-    const ui = new UI();
-    ui.playSong(event.target)
-});
-
-document.querySelector("#print").addEventListener("click", (event) => {
-    const ui = new UI();
-    ui.loveSong(event.target)
-    /* ui.addLoveSong() */
-});
-
-document.querySelector("#print").addEventListener("click", (event) => {
-   const ui = new UI();
-   ui.delete(event.target)
-  /*  ui.prints(); */
-   /* console.log(ui.delete()); */
-
-});
+let m = new Main();
