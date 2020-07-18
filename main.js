4865e59dc178c00c15d45e55e8c33f5653baf173
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2020-07-17 19:40:51 -0500

    avance

--- main.js
+++ main.js
@@ -15,15 +15,15 @@ class UI {
         element.innerHTML = `
                 <p class="cancionTitle">Canción</p>
                 <p class="cancionName" id="cancionName">${song.name}</p>
-                <img class="img" src="musica.png">
+                <img class="img" src="imgs/musica.png">
                 <div class="agregadasInfo">
                     <p id="cancionInfo">${song.author}-${song.year}</p>
                 </div>
                 <div class="actions">
-                        <button class="btns mover"><img class="logos" id="btnLove"  data-loving="${song.name}-${song.author}" name="love" src="heart.png"></button>
-                        <button  class="btns" id="btnPlay"><img data-nombre="Reproduciendo...${song.name}" class="logos" name="play" src="play.png"></button>
+                        <button class="btns mover"><img class="logos" id="btnLove"  data-loving="${song.name}-${song.author}" name="love" src="imgs/heart.png"></button>
+                        <button  class="btns" id="btnPlay"><img data-nombre="Reproduciendo...${song.name}" class="logos" name="play" src="imgs/play.png"></button>
                 </div>
-                <button class="btnDelete"><img class="logoDelete" src="delete.png"></button>
+                <button class="btnDelete"><img class="logoDelete" name="delete" src="imgs/delete.png"></button>
         `;
         songList.appendChild(element)
     }
@@ -32,13 +32,17 @@ class UI {
         document.querySelector("#form").reset();
     }
 
+
     playSong(element) {
+        console.log(element);
         let playing = document.querySelector("#playing");
         if (element.name == "play") {
             playing.innerHTML = `${element.dataset.nombre} <img class="playing-img" src="playing.png">`; /* dataset es para poder manejar los datos de los atributos personalizados */
         }
     }
+    
     loveSong(element) {
+        console.log(element.parentElement.parentElement);
         let loving = document.querySelector("#loving");
         if(element.name == "love") {
             loving.innerHTML += `<img class="cora" src="heart.png"> ${element.dataset.loving} <br> `;
@@ -51,6 +55,20 @@ class UI {
         element.innerHTML = `${this.loveSong(element)}`;
         lovesList.appendChild(element);
     } */
+    
+    delete(element) {
+        if(element.name == "delete") {
+            element.parentElement.parentElement.remove();
+            this.prints();
+        }
+    }
+
+    prints() {
+        let playing = document.querySelector("#playing");
+        let loving = document.querySelector("#loving");
+        playing.innerHTML = "";
+        loving.innerHTML = "";
+    }
 }
 
 document.querySelector("#btnAgregar").addEventListener("click", () => {
@@ -72,4 +90,12 @@ document.querySelector("#print").addEventListener("click", (event) => {
     const ui = new UI();
     ui.loveSong(event.target)
     /* ui.addLoveSong() */
-})
+});
+
+document.querySelector("#print").addEventListener("click", (event) => {
+   const ui = new UI();
+   ui.delete(event.target)
+  /*  ui.prints(); */
+   /* console.log(ui.delete()); */
+
+});
